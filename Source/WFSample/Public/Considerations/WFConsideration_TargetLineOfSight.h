d7c0d791ef7f8b51037845a36d6ebfb0f94f3330
Shayan Zamiri <shayan.zamiri@gmail.com>
2023-06-12 19:05:57 +0330

    Changed the email address on top of files.

--- Source/WFSample/Public/Considerations/WFConsideration_TargetLineOfSight.h
+++ Source/WFSample/Public/Considerations/WFConsideration_TargetLineOfSight.h
@@ -1,6 +1,6 @@
 ﻿/***************************************************************************************/
 /* Copyright (c) 2023 NoOp Army. All rights reserved. */
-/* Written by Shayan Zamiri <shayan.zamiri@gmail.com> */
+/* Written by Shayan Zamiri <support@nooparmygames.com> */
 /***************************************************************************************/
 
 #pragma once
