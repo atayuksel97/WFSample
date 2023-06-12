a3fbbffbb1cc998fcb5b88f77272d603f1e0c7e8
Shayan Zamiri <shayan.zamiri@gmail.com>
2023-06-12 19:17:46 +0330

    Added twitter handle.

--- Source/WFSample/Private/WFPlayerCharacter.cpp
+++ Source/WFSample/Private/WFPlayerCharacter.cpp
@@ -1,6 +1,7 @@
 /***************************************************************************************/
 /* Copyright (c) 2023 NoOp Army. All rights reserved. */
 /* Written by Shayan Zamiri <support@nooparmygames.com> */
+/* Author's twitter: @Shayan_Zamiri */
 /***************************************************************************************/
 
 
--- Source/WFSample/Private/WFWeapon.cpp
+++ Source/WFSample/Private/WFWeapon.cpp
@@ -1,6 +1,7 @@
 ﻿/***************************************************************************************/
 /* Copyright (c) 2023 NoOp Army. All rights reserved. */
 /* Written by Shayan Zamiri <support@nooparmygames.com> */
+/* Author's twitter: @Shayan_Zamiri */
 /***************************************************************************************/
 
 #include "WFWeapon.h"
